ddbab84bec9724363e3639d2f09c5250f9df6aa6
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 21:42:32 +0300

    Importing Technologies component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Technologies from './components/Technologies'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Navbar /> 
         <Hero />
         <About />
+        <Technologies />
       </div>
     </div>
   )
