e7a0f7d4aff9b48c98ad71b7f549155e1c451b7f
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 19:27:50 +0300

    Creating navbar with react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center p-2'>
+            <h1 className='text-white text-2xl'>AA</h1>
+        </div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub />
+            <FaSquareXTwitter />
+            <FaInstagram />
+        </div>
+    </nav>
   )
 }
 
