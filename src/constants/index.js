42cc4b5783b7be9c5b829b02e8dd624f65d5d27a
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 19:05:48 +0300

    Initailizing constants

---
+++ src/constants/index.js
@@ -0,0 +1,40 @@
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React , as well as back-end technologies like MySQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, MySQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Indigoflix Movies",
+    description:
+      "A modern, responsive movie discovery app built with React, Vite, and Tailwind CSS — search thousands of movies with real-time results powered by The Movie DB API.",
+    technologies: ["HTML", "Tailwind CSS", "React", "API"],
+  },
+  {
+    title: "React Calculator",
+    description:
+      "A simple calculator app built with React and Tailwind CSS.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
+  },
+  {
+    title: "Coding Quiz App",
+    description:
+      "Coding Quiz App — A simple React web app to test your programming knowledge in Python, JavaScript, and General Coding concepts.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
+  },
+  {
+    title: "Pump Plan",
+    image: project4,
+    description:
+      "Generate personalized workout plans with targeted exercises using JavaScript and the API Ninjas fitness API.",
+    technologies: ["HTML", "CSS", "API", "JavaScript", "Firebase Auth"],
+  },
+];
+
+export const CONTACT = {
+  address: "Lebanon - Nabateye main road",
+  phoneNo: "+961 81 090 757",
+  email: "adamabdallahayln1@gmail.com",
+  github: "AdamAbdallah1",
+  instagram: "ciaoace",
+  linkedin: "Adam Abdallah"
+};
