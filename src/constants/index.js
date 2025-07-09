cc4a192fa1b2478c34c5cc2242368961cad164d9
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 22:24:15 +0300

    Importing projects images:

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+import project1 from "../assets/projects/project-1.png"
+import project2 from "../assets/projects/project-2.png"
+import project3 from "../assets/projects/project-3.png"
+import project4 from "../assets/projects/project-4.png"
+
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React , as well as back-end technologies like MySQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, MySQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
