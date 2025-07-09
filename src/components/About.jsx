c9aa6f1b010ca2125cf35b2e1c4842152044e59f
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-10 01:28:31 +0300

    Fixing sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ function About() {
             initial={{ opacity: 0, x: 100}}
             className='w-full lg:w-1/2'>
                 <div className='flex justify-center lg:justify-start'>
-                    <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+                    <p className='my-2 max-w-xl py-6 lg:text-[20px]'>{ABOUT_TEXT}</p>
                 </div>
             </motion.div>
         </div>
