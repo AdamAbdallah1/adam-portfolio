4cf35c857f3225c6555d142c8c67a7cec1aac2d0
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 22:22:11 +0300

    Fixing sizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start mt-[-100px]'>
-                    <h1 className='pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Adam Abdallah</h1>
+                    <h2 className='pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Adam Abdallah</h2>
                     <span className='bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</span>
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
                 </div>
