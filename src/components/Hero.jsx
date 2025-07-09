cffca394c0721500b67d130319b878678b0e1a48
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 20:57:10 +0300

    Adding profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { HERO_CONTENT } from '../constants/index.js'
+import profilepic from '../assets/AdamAbdallahProfile.png'
 
 function Hero() {
   return (
@@ -6,7 +8,14 @@ function Hero() {
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1>Adam Abdallah</h1>
+                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Adam Abdallah</h1>
+                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</span>
+                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+                </div>
+            </div>
+            <div className='w-full lg:w-1/2 lg:p-8'>
+                <div className='flex justify-center'>
+                    <img className='lg:w-130 lg:mt-[-290px]  w-70 mt-[-150px]' src={profilepic} alt="Adam Abdallah Profile Pic" />
                 </div>
             </div>
         </div>
