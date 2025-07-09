c9e5b06f8dc999d85a7f0c2096f8fbfeba84857e
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 23:16:46 +0300

    Fixing margin and sizzing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ function Hero() {
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start mt-[-100px]'>
-                    <h2 className='pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Adam Abdallah</h2>
-                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</span>
+                    <h2 className='mt-[15px] pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Adam Abdallah</h2>
+                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
                 </div>
             </div>
             <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                    <img className='lg:w-100 lg:mt-[-225px]  w-70 mt-[-150px]' src={profilepic} alt="Adam Abdallah Profile Pic" />
+                    <img className='lg:w-100 lg:mt-[-225px]  w-70 mt-[-130px]' src={profilepic} alt="Adam Abdallah Profile Pic" />
                 </div>
             </div>
         </div>
