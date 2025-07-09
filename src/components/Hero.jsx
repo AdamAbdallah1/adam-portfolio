59dcf3f633a7ae733e350b938d31c423375af358
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 19:31:42 +0300

    Creating some divs and styling with tailwind

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='-border-b border-neutral-900 pb-4 lg:mb-35'>
+        <div className='flex flex-wrap'>
+            <div className='w-full lg:w-1/2'>
+                <div className='flex flex-col items-center lg:items-start'>
+                    <h1>Adam Abdallah</h1>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
