fad45550b3e17efc0090a648f53235cabe67ec3b
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-09 19:38:01 +0300

    Changing name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center p-2'>
-            <h1 className='text-white text-2xl'>AA</h1>
+            <h1 className='text-white text-2xl'>Aa</h1>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin />
